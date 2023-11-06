41f946898cad1a7fe87e4868d63be27d83d1b20b
Андрей Барышников <andrey1andrey@narod.ru>
2023-11-06 14:28:02 +0300

    Initial commit

---
+++ src/main/java/ru/andreybaryshnikov/otus_2_14/Otus214Application.java
@@ -0,0 +1,13 @@
+package ru.andreybaryshnikov.otus_2_14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Otus214Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Otus214Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/andreybaryshnikov/otus_2_14/Otus214ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andreybaryshnikov.otus_2_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Otus214ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
